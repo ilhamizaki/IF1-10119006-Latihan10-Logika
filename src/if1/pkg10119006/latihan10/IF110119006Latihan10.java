ffab9971c9639397de9a4e25078c326dfb97f9f4
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 09:02:09 +0700

    fix

---
+++ src/if1/pkg10119006/latihan10/IF110119006Latihan10.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan10;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Tampil Logika
+ */
+public class IF110119006Latihan10 {
+
+    public static void main(String[] args) {
+       int umur = 19;
+       boolean dewasa;
+       dewasa = (umur >= 17);
+       System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
